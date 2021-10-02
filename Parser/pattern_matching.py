ed1cc8b9228751debc4d817495161879f0fc60a6
blankRiot96 <veresharjun5@gmail.com>
2021-10-02 22:07:13 +0530

    Regex successfully combined with lexer

--- Parser/pattern_matching.py
+++ Parser/pattern_matching.py
@@ -5,8 +5,18 @@ while_loop = re.compile(r'while (.*) ({.*})')
 
 
 def condition_evaluation(pattern: re.Pattern, lexem: str) -> tuple[str, str]:
+    # Matching the pattern according to the type of statement
+    # and returning the condition and code to be executed(evaluation)
+    # Example: lexem = "if (condition) {evaluation}"
+    # mo.group(1) = "(condition)"
+    # mo.group(2) = "{evaluation}"
     mo = pattern.search(lexem)
     condition = mo.group(1)
     evaluation = mo.group(2)
+
+    # Striping of parenthesis
+    # Example: "(condition)" -> "condition"
+    condition = condition[1:-1]
+    evaluation = evaluation[1:-1]
 
     return condition, evaluation
